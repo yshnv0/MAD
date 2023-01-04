82ed1c99824bd8ba0d7d4aeeefc53cb95adc893e
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2023-01-04 21:54:58 +0530

    Create MainActivity.java

---
+++ Themes/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click(View view)
+    {
+        Intent i=new Intent(getApplicationContext(), com.example.myapplication.MainActivity2.class);
+        startActivity(i);
+    }
+}
